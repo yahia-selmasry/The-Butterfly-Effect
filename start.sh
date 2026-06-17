6e25c5579280a8c54bee099e513eb635560c84f4
yahia-elmasry-27 <yahia.sh.elmasry@gmail.com>
2026-06-17 15:07:03 -0700

    Use absolute Python path in start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-python3 -m pip install -r requirements.txt --quiet
-python3 -m gunicorn app:app --bind 0.0.0.0:8080
+/home/runner/workspace/.pythonlibs/bin/python -m pip install -r requirements.txt --quiet
+/home/runner/workspace/.pythonlibs/bin/python -m gunicorn app:app --bind 0.0.0.0:8080
